2df2a2106932e229ec64309450c7dd3a548dfeb3
skv221 <www.venkatesan.skv@gmail.com>
2022-03-20 12:36:57 +0530

    supplying forms is done with backend

--- src/app/components/generate/generate.component.ts
+++ src/app/components/generate/generate.component.ts
@@ -11,5 +11,22 @@ export class GenerateComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  buttonText="Click to copy";
+  copyMsg=`Dear customer, Thank you for utilising our service. We need your feedback in order to improve our service. Kindly submit your feedback in the following link. 
+  
+http://localhost:4200/feedback`;
+  copyMessage(val: string){
+    const selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    this.buttonText="Copied!!"
+    document.body.removeChild(selBox);
+  }
 }
