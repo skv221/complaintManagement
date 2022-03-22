a852525e90c5a5d5a67327075d7b26a52b60b7eb
skv221 <www.venkatesan.skv@gmail.com>
2022-03-22 20:33:51 +0530

    Positive feedback page done

--- src/app/components/positive/positive.component.ts
+++ src/app/components/positive/positive.component.ts
@@ -11,5 +11,26 @@ export class PositiveComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  reviews=[
+    {
+      name:"Venkatesan S K",
+      email:"www.venkatesan.skv@gmail.com",
+      feedback:`The service is awesome and I can't find any problems. Please continue the same kind of service in future.`
+    },
+    {
+      name:"Pranav Shiva P",
+      email:"kuttyshiva2000@gmail.com",
+      feedback:`The service is awesome and I can't find any problems. Please continue the same kind of service in future.`
+    },
+    {
+      name:"Sai Anirudh K",
+      email:"amazinganirudhhere@gmail.com",
+      feedback:`The service is awesome and I can't find any problems. Please continue the same kind of service in future.`
+    },
+    {
+      name:"Nathan M",
+      email:"nathanmcse@citchennai.net",
+      feedback:`The service is awesome and I can't find any problems. Please continue the same kind of service in future.`
+    }
+  ]
 }
