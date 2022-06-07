2eaccdf42c475a0a9acf2b292e64b6d5f9b666ba
skv221 <www.venkatesan.skv@gmail.com>
2022-06-07 15:07:56 +0530

    80% done

--- src/app/components/negative/negative.component.ts
+++ src/app/components/negative/negative.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FeedbackService } from 'src/app/feedback.service';
+import { Institute } from 'src/app/institute';
 
 @Component({
   selector: 'app-negative',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NegativeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _feedbackService: FeedbackService, private route: ActivatedRoute) { }
+
+  loggedInstitute!: Institute;
+  instituteID!: string | null;
+  reviews!: any[];
 
   ngOnInit(): void {
+    let id = this.route.snapshot.paramMap.get('id');
+    this.instituteID = id;
+    this._feedbackService.getInstitute(this.instituteID)
+    .subscribe(data=>{
+      this.loggedInstitute=data
+      this.reviews=this.loggedInstitute.complaints.filter(user=>user.analysis.score<0);
+    });
   }
 
 }
